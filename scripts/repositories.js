5e0820fed27a44be30350f586e8b5c9b8696cf9b
Oshimani <jannick2.0@googlemail.com>
2020-10-07 20:16:29 +0200

    repository cards done

--- scripts/repositories.js
+++ scripts/repositories.js
@@ -1,14 +1,73 @@
+const renderRepo = (repo, index) => {
+    return `
+<!-- REPO -->
+<div class="repo-card">
+    <!-- HEADER -->
+    <header>
+        <div class="number">${index + 1}</div>
+        ${repo.language ?
+            `<div class="language">${repo.language}</div>`
+            : ""
+        }
+        <div class="title">${repo.name}</div>
+    </header>
+
+    <!-- CONTENT -->
+    <section class="content">
+       ${repo.description || ""}
+    </section>
+
+    <!-- SPACER -->
+    <div class="spacer"></div>
+    
+    <!-- METADATA -->
+    <section class="metadata">
+        <div class="created">
+            Created: <span>${new Date(repo.created_at).toLocaleDateString()}</span>
+        </div>
+        <div class="commited">
+            Last Commit: <span>${new Date(repo.pushed_at).toLocaleDateString()}</span>
+        </div>
+    </section>
+
+    <!-- FOOTER -->
+    <footer>
+        <a target="_blank" href="${repo.html_url}">
+            Check out on GitHub
+        </a>
+        <a download href="${repo.html_url}/zipball/master/">
+            Download Source Code
+        </a>
+    </footer>
+</div>
+`
+}
+
+const renderRepos = (repos) => {
+
+    //sort repos
+    // todo
+
+    // render each repo
+    document.getElementById('repos-anchor').innerHTML = repos.map((repo, index) => {
+        return renderRepo(repo, index)
+    }).join("")
+}
+
 const loadRepos = () => {
-    fetch('https://api.github.com/users/Oshimani/repos')
+    fetch('https://api.github.com/users/Oshimani/repos?sort=updated')
 
-        .then(repos => {
-            console.log('Success fetching repos! Found: ', repos.length);
-
+        // success
+        .then(result => {
+            // read body stream
+            result.json()
+                .then(
+                    repos => {
+                        console.log('Success fetching repos! Found: ', repos.length)
+                        renderRepos(repos)
+                    }
+                )
         })
 
         .catch(error => console.error('Failed to fetch repos!', error))
-}
-
-const renderRepo=(repo)=>{
-    
 }
